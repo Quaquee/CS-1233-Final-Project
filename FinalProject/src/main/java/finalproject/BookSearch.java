1ff505254fe215cc63685f455ce81d3c5e9a93b0
keizhun <ethankeith876@gmail.com>
2026-04-14 13:27:08 +0800

    Book Search class

---
+++ FinalProject/src/main/java/finalproject/BookSearch.java
@@ -0,0 +1,21 @@
+package finalproject;
+
+import java.util.ArrayList;
+
+public class BookSearch {
+
+    public static void searchBook(String title, ArrayList<Book> booksArr) {
+
+        for (Book book : booksArr) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                System.out.println("Book found! ");
+                System.out.println("Title: " + book.getTitle());
+                System.out.println("Author: " + book.getAuthor());
+                System.out.print("Pages: " + book.getPages());
+
+                return;
+            }
+        }
+        System.out.println("Book not found!");
+    }
+}
