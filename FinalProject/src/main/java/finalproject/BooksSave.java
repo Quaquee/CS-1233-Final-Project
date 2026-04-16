ae270eb61c3a54d82cf30597c157911bb7fac54a
Quaquee <ryoung@addu.edu.ph>
2026-04-16 20:18:58 +0800

    Fixed Saving

--- FinalProject/src/main/java/finalproject/BooksSave.java
+++ FinalProject/src/main/java/finalproject/BooksSave.java
@@ -5,12 +5,15 @@ import java.io.FileNotFoundException;
 
 public class BooksSave {
     public static void saveBooks() throws FileNotFoundException {
-        PrintWriter outFile = new PrintWriter("studentRecordTEST.txt");
-        outFile.println("//[TITLE];[AUTHOR];[PAGECOUNT];[CATEGORY];[BORROWED?]");
+        PrintWriter outFile = new PrintWriter("booksTEST.txt");
         for (Book book : Storage.booksArr) {
             outFile.print(book.getTitle() + ";");
             outFile.print(book.getAuthor() + ";");
             outFile.print(book.getPages() + ";");
+            outFile.print(book.getAvailability());
+            outFile.println();
+            System.out.println("Saved successfully!");
         }
+        outFile.close();
     }
 }
