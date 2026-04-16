8a7669d6930e2fd9c49f4cf775067d253520353f
Ry-ee <searsryan17@gmail.com>
2026-04-16 12:30:19 +0800

    Update BookView.java

--- FinalProject/src/main/java/finalproject/BookView.java
+++ FinalProject/src/main/java/finalproject/BookView.java
@@ -1,18 +1,14 @@
 package finalproject;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class BookView {
     public void displayBooks() {
-        try{
-            for (Book book : Storage.booksArr){
-                System.out.println("Title: " + book.getTitle());
-                System.out.println("Author: " + book.getAuthor());
-                System.out.println("Pages: " + book.getPages());
-                System.out.println("----------");
-            }   
-        } catch (IOException e) {
-            System.out.println("Error reading file: " + e.getMessage());
-        }
+        for (Book book : Storage.booksArr){
+            System.out.println("Title: " + book.getTitle());
+            System.out.println("Author: " + book.getAuthor());
+            System.out.println("Pages: " + book.getPages());
+            System.out.println("----------");
+        }   
     }
 }
