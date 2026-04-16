14398bbac1435178e497bda15936e29d243c51de
Ry-ee <searsryan17@gmail.com>
2026-04-16 12:26:08 +0800

    Refactored to read from booksArr
    Refactored displayBooks method to use Storage.booksArr instead of reading from a file.

--- FinalProject/src/main/java/finalproject/BookView.java
+++ FinalProject/src/main/java/finalproject/BookView.java
@@ -1,23 +1,16 @@
 package finalproject;
 
 import java.util.*;
-import java.io.*;
 
 public class BookView {
     public void displayBooks() {
         try{
-            BufferedReader read = new BufferedReader(new FileReader("books.txt"));
-            String line;
-
-            while ((line = read.readLine()) != null){
-                String[] parts = line.split(";");
-                Books book = new Books(parts[0], parts[1], Integer.parseInt(parts[2]));
+            for (Book book : Storage.booksArr){
                 System.out.println("Title: " + book.getTitle());
                 System.out.println("Author: " + book.getAuthor());
                 System.out.println("Pages: " + book.getPages());
                 System.out.println("----------");
-            }
-            read.close();    
+            }   
         } catch (IOException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
