9a434b91a35c93f982347e64663eca8cb41c8886
Quaquee <ryoung@addu.edu.ph>
2026-04-19 00:24:54 +0800

    consistency 2

--- FinalProject/src/main/java/finalproject/BookAdd.java
+++ FinalProject/src/main/java/finalproject/BookAdd.java
@@ -15,6 +15,6 @@ public class BookAdd {
         Book book = new Book(title, author, pages, category, true);
         Storage.booksArr.add(book);
         Storage.booksArr.sort(Comparator.comparing(Book::getTitle, String.CASE_INSENSITIVE_ORDER));
-        System.out.println("Book successfully added to inventory.");
+        System.out.printf("%s successfully added to inventory.%n", title);
     }
 }
